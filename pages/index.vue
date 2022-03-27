c61c6e6e778db4d8b1ae813f78caafe8494a79b7
mostafa7904 <mostafa7904@gmail.com>
2022-03-27 13:23:40 +0430

    add the reproduction code

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-card
+        link
+        href="https://example.com"
+        flat
+        color="white"
+        class="mx-auto"
+        max-width="400"
+        rounded="xl"
+      >
+        <v-img contain max-height="250" src="/test.avif"> </v-img>
+        <v-card-subtitle class="text-right">
+          <v-chip color="primary" class="ml-2" small> # Tutorial </v-chip>
+          <v-chip color="primary" small> # Blog </v-chip>
+        </v-card-subtitle>
+        <v-card-text class="text-right">
+          <a
+            class="text-decoration-none blog-post-title text--primary"
+            href="https://example.com"
+          >
+            The link
+          </a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
